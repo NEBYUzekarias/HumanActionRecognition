9446135dde04a506da3e4b000f7a46e33118c10b
Mitiku <se.mitiku.yohannes@gmail.com>
2018-03-16 13:09:45 +0300

    added util methos

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -45,6 +45,72 @@ class HumanActionDataset(object):
             test_df = pd.DataFrame(columns=["sequence"])
             test_df["sequence"] = test
             test_df.to_pickle(os.path.join(self.dataset_path,"test.pkl"))
+    def split_array(self,array,max_size):
+        """Split given array to smaller array with maximum array length of max_size.
+        If len(array)%max_size ==0 all arrays will have the same length(max_size), other 
+        wise all arrays will have the same length(max_size) except the last one. It will contain
+        len(array)%max_size.
+        Arguments:
+            array {list} -- List that will be splitted.
+            max_size {int} -- Maximum allowable output array length
+        
+        Returns:
+            list -- list containing splitted list
+        """
+
+        assert max_size>0, "max_size should be greater than 0"
+        if max_size>=len(array):
+            return array
+        output = []
+        for i in range(0,len(array),max_size):
+            output+=[array[i:i+max_size]]
+        return output
+    def __split_sequence_helper(self,sequence,max_sequence_length):
+        current_path  = os.path.join(self.dataset_path,sequence)
+        s_images = os.listdir(current_path)
+        s_images.sort()
+        splited_seq = self.split_array(s_images,max_sequence_length) 
+        output={}
+        for i in range(len(splited_seq)):
+            output[sequence+"_"+str(i)] = []
+            for image_file in splited_seq[i]:
+                output[sequence+"_"+str(i)].append(image_file)
+        return output
+    def __save_splitted_sequences(self,origin_dir,sequences_dict,output_path):
+        for sequence in sequences_dict:
+            if not os.path.exists(os.path.join(output_path,sequence)):
+                os.mkdir(os.path.join(output_path,sequence))
+            for img_file in sequences_dict[sequence]:
+                shutil.copy(os.path.join(origin_dir,img_file),os.path.join(output_path,sequence,img_file))
+            
+    def split_all_sequences(self,output_path):
+        train_df = pd.read_pickle(os.path.join(self.dataset_path,"train_all.pkl"))
+        test_df = pd.read_pickle(os.path.join(self.dataset_path,"test_all.pkl"))
+        validation_df = pd.read_pickle(os.path.join(self.dataset_path,"validation_all.pkl"))
+        train_small_sequences = {}
+
+        if not os.path.exists(output_path):
+            os.mkdir(output_path)
+        if not os.path.exists(os.path.join(output_path,"train")):
+            os.mkdir(os.path.join(output_path,"train"))
+            
+        if not os.path.exists(os.path.join(output_path,"test")):
+            os.mkdir(os.path.join(output_path,"test"))
+        if not os.path.exists(os.path.join(output_path,"validation")):
+            os.mkdir(os.path.join(output_path,"validation"))
+
+
+        for sequence in train_df["sequence"]:
+            smaller_sequences = self.__split_sequence_helper(sequence,30)
+            self.__save_splitted_sequences(os.path.join(self.dataset_path,sequence),smaller_sequences,os.path.join(output_path,"train"))
+        for sequence in test_df["sequence"]:
+            smaller_sequences = self.__split_sequence_helper(sequence,30)
+            self.__save_splitted_sequences(os.path.join(self.dataset_path,sequence),smaller_sequences,os.path.join(output_path,"test"))
+        for sequence in validation_df["sequence"]:
+            smaller_sequences = self.__split_sequence_helper(sequence,30)
+            self.__save_splitted_sequences(os.path.join(self.dataset_path,sequence),smaller_sequences,os.path.join(output_path,"validation"))
+
+
     
     def load_dataset(self,uses_generator=False):
         pass
--- train/__main__.py
+++ train/__main__.py
@@ -1,7 +1,8 @@
 from dataset import HumanActionDataset
 def main():
     d = HumanActionDataset("/dataset/human-action/images",(48,48,1),uses_generator=False)
-    d.split_train_test()
+    # d.split_train_test()
+    d.split_all_sequences("/dataset/ha/")
 
 if __name__ == '__main__':
     main()
